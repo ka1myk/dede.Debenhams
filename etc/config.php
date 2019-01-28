a90ce5780fdd65bfc72512e604e844d8cc14ccc2
ka1myk <kalmyk.mp3@gmail.com>
2019-01-28 13:00:04 +0300

    01/28/19 13:00:04

--- etc/config.php
+++ etc/config.php
@@ -14,7 +14,7 @@ return [
         'Magento_AdminNotification' => 1,
         'Magento_Indexer' => 1,
         'MSP_ReCaptcha' => 1,
-        'MX_MegaMenu' => 1,
+        'MX_MegaMenu' => 0,
         'MageKey_StoresFlags' => 1,
         'Magento_Email' => 1,
         'MageWorx_AlsoBought' => 1,
