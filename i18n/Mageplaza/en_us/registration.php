1fc41cdfc127902b7b268e677435b121e67f794b
ka1myk <kalmyk.mp3@gmail.com>
2019-02-19 11:40:03 +0300

    02/19/19 11:40:03

--- i18n/Mageplaza/en_us/registration.php
+++ i18n/Mageplaza/en_us/registration.php
@@ -16,6 +16,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'mageplaza_et_ee',
+    'mageplaza_en_us',
     __DIR__
 );
