3a10cb9cf968d1ba2c91c50dc111f26f7e139ad8
ka1myk <kalmyk.mp3@gmail.com>
2019-03-24 11:00:03 +0300

    03/24/19 11:00:03

--- code/MX/MegaMenu/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'MX_MegaMenu',
-    __DIR__
-);
--- etc/config.php
+++ etc/config.php
@@ -14,7 +14,6 @@ return [
         'Magento_AdminNotification' => 1,
         'Magento_Indexer' => 1,
         'MSP_ReCaptcha' => 1,
-        'MX_MegaMenu' => 0,
         'MageKey_StoresFlags' => 1,
         'Magento_Email' => 1,
         'MageWorx_AlsoBought' => 1,
